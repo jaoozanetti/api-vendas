5e801e13a458fb46898d62d7cb41963024bac908
João Pedro Caetano <joaopedrocaetano07032001@gmail.com>
2026-01-30 17:12:10 -0300

    Feature: Add Client and Product entities to Sales module imports

--- src/modules/sales/sales.module.ts
+++ src/modules/sales/sales.module.ts
@@ -4,9 +4,11 @@ import { SalesController } from './sales.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Sale } from './entities/sale.entity';
 import { SaleItem } from './entities/sale-item.entity';
+import { Product } from '../products/entities/product.entity';
+import { Client } from '../clients/entities/client.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Sale, SaleItem])],
+  imports: [TypeOrmModule.forFeature([Sale, SaleItem, Client, Product])],
   controllers: [SalesController],
   providers: [SalesService],
 })
