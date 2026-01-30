7d5dc99d059cb9659e1617939c7a95495df6ba3c
João Pedro Caetano <joaopedrocaetano07032001@gmail.com>
2026-01-30 15:28:17 -0300

    Feature: Teste e ajustes no módulo de produtos, tudo ok

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,26 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+// Serviço responsável por gerenciar os produtos
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  // 1ª função: Criar um novo produto
+async create(createProductDto: CreateProductDto) {
+  const product = this.productRepository.create(createProductDto);
+  return await this.productRepository.save(product);
+}
+
+// 2ª função: Retornar todos os produtos
+  async findAll() {
+    return this.productRepository.find(); // Retorna uma cópia do array de produtos
   }
 
-  findAll() {
-    return `This action returns all products`;
+  // 3ª função: Retornar um produto pelo ID
+  async findOne(id: number) {
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product) {
+      return { message: `Produto com ID ${id} não encontrado, digite um ID válido` };
+    }
+    return product;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  // 4ª função: Atualizar um produto pelo ID
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product) {
+      return { message: `Produto com ID ${id} não encontrado, digite um ID válido` };
+    }
+    Object.assign(product, updateProductDto, { updatedAt: new Date() });
+    return this.productRepository.save(product);
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  // 5ª função: Remover um produto pelo ID
+  async remove(id: number) {
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product) {
+      return { message: `Produto com ID ${id} não encontrado, digite um ID válido` };
+    }
+    await this.productRepository.softDelete(id);
+    return { message: `Produto com ID ${id} removido com sucesso` };
   }
 }
