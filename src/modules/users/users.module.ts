3f614f3996fdf2cbf0499a053cf57b7634f2d4c7
João Pedro Caetano <joaopedrocaetano07032001@gmail.com>
2026-02-20 14:50:27 -0300

    Fix: Ajuste Logs

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm'; // Importa o módulo TypeORM pa
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity'; // Importa a entidade User para registrar no TypeORM
+import { LogsModule } from '../logs/logs.module';
 
 @Module({
   // Registra a entidade User no módulo TypeORM para que possa ser usada no serviço
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), LogsModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService], // Exporta o UsersService para que possa ser usado em outros módulos (como o AuthModule)
